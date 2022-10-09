6cdac0e220fdb970c002476ce4ba951d0010ccd4
KaiLee85 <87049080+KaiLee85@users.noreply.github.com>
2022-10-09 17:00:50 -0600

    request data

--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -1,7 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+
+  //A snippet of code which runs based on a specific condition/varaible
+  useEffect(() => {
+    //if [], run once when the row loads, dont run again
+    async function fetchData() {
+      //wait for answer back then request
+      const request = await axios.get(fetchUrl);
+      console.log(request);
+      return request;
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h2>{title}</h2>
--- netflix/src/requests.js
+++ netflix/src/requests.js
@@ -1,14 +1,14 @@
-const API_KEY = process.env.TMDB_API;
+const API_KEY = process.env.REACT_APP_TMDBAPI;
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: `/trending/all/day?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchActionMovies: `/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchComedyMovies: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchHorrorMovies: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchRomanceMovies: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchDocumantaries: `/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
 };
 
 export default requests;
